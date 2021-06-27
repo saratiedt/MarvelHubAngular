abcd14216b87999a843d2f8db0f7e1b90f1d7532
Igor Ruan <igor.ruangarcia@gmail.com>
2021-06-27 18:22:02 -0300

    Add StorieModel to StorieService

--- src/app/shared/services/storie.service.ts
+++ src/app/shared/services/storie.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BaseService } from './base.service';
+import { StorieModel } from '../models/storie.model';
 
 @Injectable({ providedIn: 'root' })
-export class StorieService extends BaseService {
+export class StorieService extends BaseService<StorieModel> {
   constructor(private httpClient: HttpClient) {
     super(httpClient, 'stories');
   }
