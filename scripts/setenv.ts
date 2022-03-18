e9422f0e3c2fecb58d2f17e1c35761a2f38a45bd
Igor Ruan <igor.ruangarcia@gmail.com>
2022-03-18 16:48:27 -0300

    Ajuste no script de gerar arquivos de ambiente

--- scripts/setenv.ts
+++ scripts/setenv.ts
@@ -1,10 +1,12 @@
-const { writeFile } = require('fs');
+const { writeFile, existsSync, mkdirSync } = require('fs');
 const { argv } = require('yargs');
 // read environment variables from .env file
 require('dotenv').config();
 // read the command line arguments passed with yargs
 const environment = argv.environment;
 const isProduction = environment === 'prod';
+
+const environmentsFolder = './src/environments'
 
 const enviromnentEntries = [
   'MARVEL_BASE_URL',
@@ -21,9 +23,9 @@ if (anyVarEmpty) {
   process.exit(-1);
 }
 
-const targetPath = isProduction
-  ? `./src/environments/environment.prod.ts`
-  : `./src/environments/environment.ts`;
+const devPath = `${environmentsFolder}/environment.ts`;
+const prodPath = `${environmentsFolder}/environment.prod.ts`;
+
 // we have access to our environment variables
 // in the process.env object thanks to dotenv
 const environmentFileContent = `
@@ -34,10 +36,27 @@ export const environment = {
   MARVEL_API_PRIVATE_KEY: "${process.env.MARVEL_API_PRIVATE_KEY}"
 };
 `;
-// write the content to the respective file
-writeFile(targetPath, environmentFileContent, function (err: any) {
+
+// Check if environments folder exists, if not, create it
+if (!existsSync(environmentsFolder)) {
+  console.log(`Folder: ${environmentsFolder} does not exists. Creating it!`);
+  mkdirSync(environmentsFolder);
+}
+
+// Always crate the environment.ts file
+writeFile(devPath, environmentFileContent, function (err: any) {
   if (err) {
     console.log(err);
   }
-  console.log(`Wrote variables to ${targetPath}`);
+  console.log(`Wrote variables to ${devPath}`);
 });
+
+// When prod, generate the respective file
+if (isProduction) {
+  writeFile(prodPath, environmentFileContent, function (err: any) {
+    if (err) {
+      console.log(err);
+    }
+    console.log(`Wrote variables to ${prodPath}`);
+  });
+}
